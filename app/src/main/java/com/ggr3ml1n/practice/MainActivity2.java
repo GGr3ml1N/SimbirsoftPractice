134286869f685d68d96e7a105220e42ad0de277e
Dinar Subeev <dinars163@gmail.com>
2023-01-05 14:01:26 +0400

    Демонстрация работоспособности программы

---
+++ app/src/main/java/com/ggr3ml1n/practice/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.ggr3ml1n.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Lambda lmd = new Lambda();
+        Lambda.Operationable operation = () -> Log.d("Lambda", "I Love Java!");
+        lmd.repeatTask(10, operation);
+
+        Directions up = Directions.UP;
+        Directions left = Directions.LEFT;
+        Directions down = Directions.DOWN;
+        Directions right = Directions.RIGHT;
+        Coordinate x = new Coordinate(0.0,0.0);
+        Directions.move(up, up, left, down, left, down, down, right, right, down, right);
+        x.setY(0.0);
+        x.setX(0.0);
+        Coordinate newLocation =  Directions.move(x, up);
+        Log.d("Direction", "new location is " + newLocation);
+    }
+}
