468efd0ef38bada303a6975387da59778856391c
Dinar Subeev <dinars163@gmail.com>
2023-01-08 01:51:35 +0400

    Проверка работоспособности

--- app/src/main/java/com/ggr3ml1n/practice/MainActivity.kt
+++ app/src/main/java/com/ggr3ml1n/practice/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.ggr3ml1n.practice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var book = Book(40.7, 4000)
+        var magazine = Magazine(70.3, 7500)
+        Log.d("Info", "$book and $magazine")
     }
 }
