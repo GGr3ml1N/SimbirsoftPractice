1dc52a98d6bc447141828bb6ff8795d9ddaf979d
Dinar Subeev <dinars163@gmail.com>
2023-01-13 15:29:56 +0400

    Внесено изменение в сеттер поля price

--- app/src/main/java/com/ggr3ml1n/practice/Magazine.kt
+++ app/src/main/java/com/ggr3ml1n/practice/Magazine.kt
@@ -1,19 +1,20 @@
 package com.ggr3ml1n.practice
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
-class Magazine(price : Double, wordCount : Int) : Publication {
+class Magazine(price: Double, wordCount: Int) : Publication {
     
     override var price: BigDecimal = BigDecimal(price)
-    set(value) {
-        if (value < BigDecimal.ZERO) throw IllegalArgumentException()
-        field = price
-    }
+        set(value) {
+            if (value < BigDecimal.ZERO) throw IllegalArgumentException()
+            field = price.setScale(2, RoundingMode.HALF_UP)
+        }
     override var wordCount: Int = wordCount
         set(value) {
-        if (value < 0) throw IllegalArgumentException()
-        field = wordCount
-    }
+            if (value < 0) throw IllegalArgumentException()
+            field = wordCount
+        }
     
     init {
         this.price = BigDecimal(price)
@@ -21,9 +22,8 @@ class Magazine(price : Double, wordCount : Int) : Publication {
     }
     
     override fun getType(): String = "Magazine"
+    
     override fun toString(): String {
         return "price = $price, wordCount = $wordCount"
     }
-    
-    
 }
