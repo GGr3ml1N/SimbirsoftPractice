be7e9abbe74042149474d6ca5b32e0d02f99da1f
Dinar Subeev <dinars163@gmail.com>
2023-01-17 17:54:07 +0400

    Очередная проверка работоспособности

--- app/src/main/java/com/ggr3ml1n/practice/MainActivity.kt
+++ app/src/main/java/com/ggr3ml1n/practice/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ggr3ml1n.practice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.math.BigDecimal
+
 /*
 Необходимо создать интерфейс Publication, у которого должно быть свойства – price и wordCount,
 а также метод getType, возвращающий строку. Создать два класса,
@@ -31,17 +33,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        val book = Book(40.7, 4000)
-        val magazine = Magazine(70.3, 7500)
+        val book = Book(BigDecimal.valueOf(40.7), 4000)
+        val magazine = Magazine(BigDecimal("70.3"), 7500)
         Log.d("Info", "Book's values: $book")
         Log.d("Info", "Magazine's values: $magazine")
         
-        val book2 = Book(40.7, 4000)
+        val book2 = Book(BigDecimal("40.7"), 4000)
         Log.d("Info", "Result of compare by \"===\" is ${book === book2}")
         Log.d("Info", "Result of compare by overridden equals is ${book.equals(book2)}")
         
         val book3: Book? = null
-        val book4: Book? = Book(42.4, 3000)
+        val book4: Book? = Book(BigDecimal("42.4"), 3000)
         book3?.let { buy(it) }
         book4?.let { buy(it) }
         
