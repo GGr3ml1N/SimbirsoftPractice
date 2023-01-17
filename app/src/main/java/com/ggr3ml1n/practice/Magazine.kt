04e0d0b481f5f40f6f4684fc2e32501e4a053c49
Dinar Subeev <dinars163@gmail.com>
2023-01-17 17:53:48 +0400

    Убраны методы get/set. Поля объявлены в конструкторе

--- app/src/main/java/com/ggr3ml1n/practice/Magazine.kt
+++ app/src/main/java/com/ggr3ml1n/practice/Magazine.kt
@@ -3,23 +3,7 @@ package com.ggr3ml1n.practice
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class Magazine(price: Double, wordCount: Int) : Publication {
-    
-    override var price: BigDecimal = BigDecimal(price)
-        set(value) {
-            if (value < BigDecimal.ZERO) throw IllegalArgumentException()
-            field = price.setScale(2, RoundingMode.HALF_UP)
-        }
-    override var wordCount: Int = wordCount
-        set(value) {
-            if (value < 0) throw IllegalArgumentException()
-            field = wordCount
-        }
-    
-    init {
-        this.price = BigDecimal(price)
-        this.wordCount = wordCount
-    }
+class Magazine(override var price: BigDecimal, override var wordCount: Int) : Publication {
     
     override fun getType(): String = "Magazine"
     
