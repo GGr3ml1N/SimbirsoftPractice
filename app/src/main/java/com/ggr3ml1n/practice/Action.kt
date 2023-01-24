5c16c9b065ca4ba3c84ac0e5c080e1339df5663a
Dinar Subeev <dinars163@gmail.com>
2023-01-24 16:01:16 +0400

    Оставил конструктор только у Login

--- app/src/main/java/com/ggr3ml1n/practice/Action.kt
+++ app/src/main/java/com/ggr3ml1n/practice/Action.kt
@@ -1,7 +1,8 @@
 package com.ggr3ml1n.practice
 
-sealed class Action (val user: User)  {
-    class Registration (user: User) : Action(user)
-    class Login (user: User) : Action(user)
-    class Logout (user: User) : Action(user)
-}
+sealed class Action
+class Registration : Action()
+class Login(val user: User) : Action()
+class Logout : Action()
+
+
