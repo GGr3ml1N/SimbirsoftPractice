766a346fd595e8523b2361eba0753dace009025f
Dinar Subeev <dinars163@gmail.com>
2023-01-24 16:00:00 +0400

    class -> data class

--- app/src/main/java/com/ggr3ml1n/practice/User.kt
+++ app/src/main/java/com/ggr3ml1n/practice/User.kt
@@ -2,7 +2,7 @@ package com.ggr3ml1n.practice
 
 import java.time.LocalDateTime
 
-class User(
+data class User(
     val id: Int,
     var name: String,
     var age: Int,
@@ -11,10 +11,4 @@ class User(
     val startTime: LocalDateTime by lazy {
         LocalDateTime.now()
     }
-    
-    override fun toString(): String {
-        return "User(id=$id, name='$name', age=$age, type=$type)"
-    }
-    
-    
 }
