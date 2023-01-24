2c440dfa1c8dc1dd5032a48b912abfb00ab071ab
Dinar Subeev <dinars163@gmail.com>
2023-01-24 16:53:47 +0400

    Заменил var на val

--- app/src/main/java/com/ggr3ml1n/practice/User.kt
+++ app/src/main/java/com/ggr3ml1n/practice/User.kt
@@ -4,8 +4,8 @@ import java.time.LocalDateTime
 
 data class User(
     val id: Int,
-    var name: String,
-    var age: Int,
+    val name: String,
+    val age: Int,
     val type: Type,
 ) {
     val startTime: LocalDateTime by lazy {
