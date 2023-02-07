8f49b47c635545dc5eaa41d7c7d8174f3a1956b9
Dinar Subeev <dinars163@gmail.com>
2023-02-07 18:12:42 +0400

    Реализован SplashActivity согласно макету, назначен стартовым экраном

---
+++ app/src/main/java/com/ggr3ml1n/practice/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.ggr3ml1n.practice
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreen, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
